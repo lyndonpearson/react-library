1e4ce551a2c28e9b712a0921f2ccd843384064f5
LyndonPearson <LyndonPearson@gmail.com>
2023-09-26 20:35:11 -0600

    Added Navbar to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HANDS UP GACHIBASS</h1>
-    </div>
+    <Navbar />
   );
 }
 
